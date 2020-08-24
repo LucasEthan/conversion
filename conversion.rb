e29e45b0c6a665b470f21bb027a69bd04619fb5b
Lucas Ethan <asmrit1010@gmail.com>
2020-08-24 10:55:34 +0200

    removed

--- conversion.rb
+++ conversion.rb
@@ -3,7 +3,7 @@
 # Asks the user for temperature
 print "Enter the temperature in Celsius: "
 celsius = gets.chomp
-fahrenheit = "#{ celsius.to_f* (9.0 / 5.0) + 32 }"
+fahrenheit =  celsius.to_f* (9.0 / 5.0) + 32 
 # Converts in Fahrenheit
 puts(format("The temperature in degrees Fahrenheit is %.2f   ", "#{fahrenheit}" ))
 
