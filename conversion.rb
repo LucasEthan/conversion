774f33903d735f8d94a1b875c68fe7a16599c91b
Lucas Ethan <asmrit1010@gmail.com>
2020-09-04 11:17:43 +0200

    Made it so it loops until you stop it

--- conversion.rb
+++ conversion.rb
@@ -1,9 +1,13 @@
-
 #!/usr/bin/env ruby
 # Asks the user for temperature
+loop do
 print "Enter the temperature in Celsius: "
 celsius = gets.chomp
 fahrenheit =  celsius.to_f * (9.0 / 5.0) + 32 
 # Converts in Fahrenheit
 puts(format("The temperature in degrees Fahrenheit is %.2f ", "#{fahrenheit}" ))
+print "Do you want to quit yes or no? [y/n]: "
+choice = gets.chomp.upcase
 
+break if choice == "Y"
+end
---
+++ conversion2.rb
@@ -0,0 +1,5 @@
+# How to read from and write to a file
+
+
+# Read Celsius input from file celsius.dat
+celsius = File.read("celsius.dat")
